bb984bffd34d9959b60712754fad41549f054c06
Ryan <e0310273@u.nus.edu>
2021-01-22 22:26:44 +0800

    yes

--- sshtunnellocal.py
+++ sshtunnellocal.py
@@ -1,5 +1,6 @@
 import paramiko
 import getpass
+import socket
 from sshtunnel import SSHTunnelForwarder
 REMOTE_SERVER_IP = 'sunfire.comp.nus.edu.sg'
 PRIVATE_SERVER_IP = '137.132.86.228'
@@ -14,12 +15,29 @@ with SSHTunnelForwarder(
     remote_bind_address=(PRIVATE_SERVER_IP, 22),
     local_bind_address=('0.0.0.0', 10022)
 ) as tunnel:
-    client = paramiko.SSHClient()
-    # client.load_system_host_keys(r'C:\Users\youca\.ssh\known_hosts')
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect('127.0.0.1', port=10022, username='xilinx', password='xilinx')
-    print(client.exec_command('mkdir SUPFMFMOEUFEF'))
-    # do some operations with client session
-    client.close()
+    # client = paramiko.SSHClient()
+    # # client.load_system_host_keys(r'C:\Users\youca\.ssh\known_hosts')
+    # client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    # client.connect('127.0.0.1', port=10022, username='xilinx', password='xilinx')
+    # print(client.exec_command('mkdir SUPFMFMOEUFEF'))
+    # # do some operations with client session
+    # client.close()
+    host = '127.0.0.1'
+    port = 10022
+        
+    mySocket = socket.socket()
+    mySocket.connect((host,port))
+        
+    message = input(" -> ")
+        
+    while message != 'q':
+            mySocket.send(message.encode())
+            data = mySocket.recv(1024).decode()
+                
+            print ('Received from server: ' + data)
+                
+            message = input(" -> ")
+                
+    mySocket.close()
 
 print('FINISH!')
