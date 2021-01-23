e71260839968d6e08867fc6d35ec66c37f5cff38
Ryan <e0310273@u.nus.edu>
2021-01-23 21:49:37 +0800

    reset

--- server.py
+++ server.py
@@ -1,36 +1,27 @@
 import socket
-
-from Cryptodome.Cipher import AES
  
 def Main():
-        host = "127.0.0.1"
-        port = 10022
-        
-        mySocket = socket.socket()
-        mySocket.bind((host,port))
-        
-        mySocket.listen(1)
-        conn, addr = mySocket.accept()
-        print ("Connection from: " + str(addr))
-
-        KEY = b'\x01\x80\xba\x82\x7a\xc8\x7a\x3a\x26\xf1\xca\xc6\x9e\x1c\x3c\xbe'
-        while True:
-                nonce, ciphertext, tag = conn.recv(1024).decode()
-                if not (ciphertext and nonce and tag):
-                        break
-                cipher = AES.new(KEY, AES.MODE_EAX, nonce=nonce)
-                plaintext = cipher.decrypt(ciphertext)
-                try:
-                        cipher.verify(tag)
-                        print("The message is authentic:", plaintext)
-                except ValueError:
-                        print("Key incorrect or message corrupted")
-                # print ("from connected  user: " + str(plaintext))
-                plaintext = plaintext.upper()
-                print ("sending: " + str(plaintext))
-                conn.send(plaintext.encode())
-                
-        conn.close()
+    host = "127.0.0.1"
+    port = 10022
+     
+    mySocket = socket.socket()
+    mySocket.bind((host,port))
+     
+    mySocket.listen(1)
+    conn, addr = mySocket.accept()
+    print ("Connection from: " + str(addr))
+    
+    while True:
+            data = conn.recv(1024).decode()
+            if not data:
+                    break
+            print ("from connected  user: " + str(data))
+            
+            data = str(data).upper()
+            print ("sending: " + str(data))
+            conn.send(data.encode())
+             
+    conn.close()
      
 if __name__ == '__main__':
     Main()
--- sshtunnellocal.py
+++ sshtunnellocal.py
@@ -3,13 +3,8 @@ import paramiko
 import getpass
 import socket
 from sshtunnel import SSHTunnelForwarder
-from Cryptodome.Cipher import AES
-
 REMOTE_SERVER_IP = 'sunfire.comp.nus.edu.sg'
 PRIVATE_SERVER_IP = '137.132.86.228'
-
-KEY = b'\x01\x80\xba\x82\x7a\xc8\x7a\x3a\x26\xf1\xca\xc6\x9e\x1c\x3c\xbe'
-cipher = AES.new(KEY, AES.MODE_EAX)
 
 username = input("Enter ssh username: ")
 password =  getpass.getpass("Enter ssh password: ")
@@ -43,14 +38,12 @@ with SSHTunnelForwarder(
         message = input(" -> ")
          
         while message != 'q':
-            ciphertext, tag = cipher.encrypt_and_digest(message)
-
-            mySocket.send(ciphertext.encode())
-            data = mySocket.recv(1024).decode()
-                
-            print ('Received from server: ' + data)
-                
-            message = input(" -> ")
+                mySocket.send(message.encode())
+                data = mySocket.recv(1024).decode()
+                 
+                print ('Received from server: ' + data)
+                 
+                message = input(" -> ")
                  
         mySocket.close()
     
