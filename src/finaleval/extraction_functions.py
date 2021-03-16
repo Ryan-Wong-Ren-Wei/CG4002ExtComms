0dd87846cfd2d6f5e529ee7c13a0c87517d662a0
unknown <e0310273@u.nus.edu>
2021-03-16 17:21:53 +0800

    extraction functions

---
+++ src/finaleval/extraction_functions.py
@@ -0,0 +1,40 @@
+import numpy as np
+from scipy.stats import pearsonr
+
+
+def get_min(data):
+    return np.min(data)
+
+
+def get_max(data):
+    return np.max(data)
+
+
+def get_std(data):
+    return np.std(data)
+
+
+def get_mean(data):
+    return np.mean(data)
+
+
+def get_iqr(data):
+    q75, q25 = np.percentile(data, [75, 25], axis=0)
+    return q75 - q25
+
+
+def get_skewness(data):
+    return data.skew()
+
+
+def get_kurtosis(data):
+    return data.kurtosis()
+
+
+def get_sma(data):
+    x, y, z = [data[col] for col in data.columns]
+    sma = 0
+    for xi, yi, zi in zip(x, y, z):
+        sma += abs(xi) + abs(yi) + abs(zi)
+    return sma
+
