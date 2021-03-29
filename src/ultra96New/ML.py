0a4724d9d7d532f2374a197bcda2809bb609bbeb
KevinAlvarez7 <33540851+KevinAlvarez7@users.noreply.github.com>
2021-03-30 02:36:18 +0800

    ML Changes and Files

--- src/ultra96New/ML.py
+++ src/ultra96New/ML.py
@@ -46,7 +46,7 @@ def eval_mlp(data, tflite_model):
 
     return (DECODE[answer])
 
-def handleML(inputQueue, output, moveCompletedFlag, evalClient):
+def handleML(dataQueues, outputForEval, globalShutDown, rdyForEval, dataQueueLock):
     try:
         # test_model = load_model("MLP")
         print("Initializing ML model")
@@ -54,26 +54,51 @@ def handleML(inputQueue, output, moveCompletedFlag, evalClient):
         tflite_model.allocate_tensors()
         print("Initialization done")
         while True:
-            dataFrame = []
-            if inputQueue.qsize() > 29: 
-                print("queue > 40, processing data")
-                for _ in range(30):
-                    dataPoint = inputQueue.get()
-                    dataPoint.pop('moveFlag')
-                    dataPoint.pop('Id')
-                    dataPoint.pop('time')
-                    dataFrame.append(dataPoint)
-                print("dataframe retrieved")
+            if globalShutDown.is_set():
+                return
+            dataFrames = [None for _ in range(NUM_DANCERS)]
+            if dataQueues[0].qsize() > 29 and dataQueues[1].qsize() > 29 and dataQueues[2].qsize() > 29: 
+                # print("queue > 40, processing data")
+                for dancerID in range(3):
+                    dataQueue = dataQueues[dancerID]
+                    dataFrame = []
+                    for _ in range(30):
+                        dataPoint = dataQueue.get()
+                        dataPoint.pop('moveFlag')
+                        dataPoint.pop('Id')
+                        dataPoint.pop('time')
+                        dataFrame.append(dataPoint)
+                    dataFrames[dancerID] = dataFrame
+                # print("dataframe retrieved")
                 # print(dataFrame)
-                pdDataFrame = pandas.DataFrame(dataFrame)
-                print(pdDataFrame)
-                data_to_evaluate = preprocess.process_data_stream(pdDataFrame)
-                prediction = eval_mlp(data_to_evaluate, tflite_model)
-                output = prediction
-                print(output)
-                evalClient.sendToEval(action=ENCODE[output],positions=1)
-                moveCompletedFlag.set()
-                while not inputQueue.empty():
-                    inputQueue.get()
+                #loadscale
+                mean = np.load("mean.npy")
+                _scale = np.load("scale.npy")
+                numMoves = [0,0,0]
+                final_prediction = -1
+                i = 0
+                for dataFrame in dataFrames:
+                    pdDataFrame = pandas.DataFrame(dataFrame)
+                    #scale
+                    data = (pdDataFrame - _mean)/_scale
+                    data_to_evaluate = preprocess.process_data_stream(data)
+                    prediction = eval_mlp(data_to_evaluate, tflite_model)
+                    numMoves[prediction] += 1
+
+                max_value = numpy.max(arr)
+                final_prediction = np.where(arr == max_value)
+
+                #prediction = random.randint(0,2)
+                output = DECODE[final_prediction]
+                outputForEval['action'] = final_prediction
+                # print(output)
+                # evalClient.sendToEval(action=ENCODE[output],positions=1)
+                rdyForEval.set()
+                dataQueueLock.set()
+                # doClockSync.set()
+                for dataQueue in dataQueues:
+                    while not dataQueue.empty():
+                        dataQueue.get()
     except:
         print(sys.exc_info())
+        return
